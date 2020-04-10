92ed236f2258774575f0163d3e4392c829ddd355
Alejandro <acabrera477@gmail.com>
2020-04-10 18:57:14 +0200

    added management for user model

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,6 +18,17 @@ class UserSerializer(serializers.ModelSerializer):
 
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """ Update a user, setting the password and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth object"""
